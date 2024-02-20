e448bbb92693ff19d3c6ba0473fade710a70a053
sunnyaditya07 <sunnyadityaa@gmail.com>
2024-02-20 17:07:10 +0530

    fixed es lint for useEffect

--- src/components/home/HeroSection.js
+++ src/components/home/HeroSection.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState } from "react";
 import "./HeroSection.scss";
 import { carouselImages, mobileCarouselImages } from "../../libs/jmData";
