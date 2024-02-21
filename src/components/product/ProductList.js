94a496bc974b51174c8b74ae8a0c411367fb0a40
sunnyaditya07 <sunnyadityaa@gmail.com>
2024-02-21 17:06:59 +0530

    added cc image

--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -3,54 +3,63 @@ import "./ProductList.scss";
 import { productData } from "../../libs/jmData";
 const ProductList = () => {
   return (
-    <div className="jm-productList-container">
-      <section className="jm-productList-section">
-        {productData.map((product) => (
-          <div className="jm-productList-item" key={product.id}>
-            <div className="jm-productList-box">
-              <img
-                src={product.image}
-                alt="product"
-                className="jp-productlist-image"
-              />
-              <div className="jm-productList-details-container">
-                <p className="jm-productList-product-title">
-                  {product.description}
-                </p>{" "}
-                <p className="jm-productList-product-price">
-                  {product.price}
-                  <span className="jm-productList-actualPrice">
-                    {product.actualPrice}
-                  </span>
-                  <span className="jm-productList-price-off">
-                    {" "}
-                    {product.off}
-                  </span>
-                </p>
+    <>
+      <div className="jm-product-list-cc-container">
+        <img
+          src={require("../../assets/cc.png")}
+          alt="cc"
+          className="jm-product-list-cc-image"
+        />
+      </div>
+      <div className="jm-productList-container">
+        <section className="jm-productList-section">
+          {productData.map((product) => (
+            <div className="jm-productList-item" key={product.id}>
+              <div className="jm-productList-box">
+                <img
+                  src={product.image}
+                  alt="product"
+                  className="jp-productlist-image"
+                />
+                <div className="jm-productList-details-container">
+                  <p className="jm-productList-product-title">
+                    {product.description}
+                  </p>{" "}
+                  <p className="jm-productList-product-price">
+                    {product.price}
+                    <span className="jm-productList-actualPrice">
+                      {product.actualPrice}
+                    </span>
+                    <span className="jm-productList-price-off">
+                      {" "}
+                      {product.off}
+                    </span>
+                  </p>
+                </div>
+                <div className="jm-productList-add-conatiner">
+                  <p className="jm-productList-add-text">Buy Now</p>
+                </div>
               </div>
-              <div className="jm-productList-add-conatiner">
-                <p className="jm-productList-add-text">Buy Now</p>
-              </div>
+              <svg
+                viewBox="0 0 256 256"
+                xmlns="http://www.w3.org/2000/svg"
+                className="jp-productList-heart-icon"
+              >
+                <rect fill="none" height="256" width="256" />
+                <path
+                  d="M128,216S28,160,28,92A52,52,0,0,1,128,72h0A52,52,0,0,1,228,92C228,160,128,216,128,216Z"
+                  fill="none"
+                  stroke="#000"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="16"
+                />
+              </svg>
             </div>
-            <svg
-              viewBox="0 0 256 256"
-              xmlns="http://www.w3.org/2000/svg"
-              className="jp-productList-heart-icon"
-            >
-              <rect fill="none" height="256" width="256" />
-              <path
-                d="M128,216S28,160,28,92A52,52,0,0,1,128,72h0A52,52,0,0,1,228,92C228,160,128,216,128,216Z"
-                fill="none"
-                stroke="#000"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="16"
-              />
-            </svg>
-          </div>
-        ))}
-      </section>
-    </div>
+          ))}
+        </section>
+      </div>
+    </>
   );
 };
 
