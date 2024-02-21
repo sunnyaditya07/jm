e3f6017b912273336cd22896c5d7bd7afb712250
sunnyaditya07 <sunnyadityaa@gmail.com>
2024-02-21 16:51:17 +0530

    added bottom bar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../reusable/Header";
 import HeroSection from "./HeroSection";
 import ProductList from "../product/ProductList";
+import Footer from "../reusable/Footer";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header />
       <HeroSection />
       <ProductList />
+      <Footer />
     </>
   );
 };
