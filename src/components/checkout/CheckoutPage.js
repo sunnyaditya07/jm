15ffb2009ed87ddc4184c81ede66dc4e47e0fb71
sunnyaditya07 <sunnyadityaa@gmail.com>
2024-02-22 18:01:49 +0530

    chckout 1

--- src/components/checkout/CheckoutPage.js
+++ src/components/checkout/CheckoutPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AddressForm from "./AddressForm";
 import Header from "../reusable/Header";
+import CheckOut from "./CheckOut";
 
 const CheckoutPage = () => {
   return (
     <>
       <Header />
-      <AddressForm />
+      {/* <AddressForm /> */}
+      <CheckOut />
     </>
   );
 };
